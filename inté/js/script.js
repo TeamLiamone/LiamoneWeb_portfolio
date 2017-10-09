5699af1755e3ba1880c3b0d10b226c619dc66482
jeremN <jeremne@gmail.com>
2017-10-09 15:52:54 +0200

    Page formation: début filter/sort by

--- inté/js/script.js
+++ inté/js/script.js
@@ -87,7 +87,6 @@ var toggleTab = {
 
 };
 
-
 /*Document*/
 $(document).ready(function() {
 
@@ -101,6 +100,28 @@ $(document).ready(function() {
 		$(this).toggleClass('toggle-open');
 
 	});
+
+	//Menu-top position fixed after hero header is out of screen
+	if( !$('.menu-top').hasClass('is-visible') ) {
+
+		var wHeight = $(window).height();
+
+		$('.menu-top').each(function() {
+
+			var togglePosition = new ScrollMagic.Scene({
+
+				triggerElement: $('main > header'),
+				offset: wHeight - 100,
+				triggerHook: 1,
+				reverse: true
+
+			})
+			.setClassToggle(this, 'is-visible')
+			.addTo(controller);
+
+		});
+
+	}
 
 	//Show-Hide Training tabs
 	if( $('main').hasClass('training') ) {
@@ -125,12 +146,76 @@ $(document).ready(function() {
 
 		});
 
+		var filterContent = window.matchMedia(' (max-width: 1199px)').matches ? $('.filter-filed') : $('.filter-cta'),
+			filterVal,
+			categoryIsAll = true;
+
+
+		/*if( window.matchMedia(' (max-width: 1199px').matches ) {
+
+			filterContent = $('.filter-field');
+
+			filterContent.on('change', function() {
+
+
+				filterVal= $(this).text();
+				console.log(filterVal);
+			});
+
+		}
+		else {
+
+			filterContent = $('.filter-cta');
+
+			filterContent.each(function() {
+
+				$(this).on('click', function(e) {
+
+					e.preventDefault();
+
+					filterVal= $(this).data('filter') || $(this).text();
+					filter(filterVal);
+					console.log(filterVal);
+
+				});
+
+			});
+
+		}*/
+
+		filterContent.on('click change', function(e) {
+
+			e.preventDefault();
+
+			filterVal = $(this).data('filter') || $(this).val();
+			filter(filterVal);
+
+			console.log(filterVal);
+		});
+
+
+		function filter(value) {
+
+			var listToFilter = $('.block-formation .row');
+
+			TweenMax.set(listToFilter, {autoAlpha: 0} );
+
+			$('.block-formation').find('div[data-category*=' +value+ ']').each(function() {
+
+				TweenMax.to(this, 0.5, {autoAlpha: 1, ease: Power1.easeInOut} );
+
+			});
+		}
+
 	}
 	//Projects thumbs reveal
-	else if( $('main').hasClass('projects') && window.matchMedia(' (min-width: 768px').matches ) {
+	else if( $('main').hasClass('projects') && window.matchMedia(' (min-width: 768px)').matches ) {
+		
 		var projectMask = $('.projects-list').find('.project-item');
 
 		projectMask.each( function() {
+
+			var textToExpand = $(this).find('.visible-text');
 
 			var expandMask = new ScrollMagic.Scene({
 
@@ -141,6 +226,7 @@ $(document).ready(function() {
 				duration: '100%'
 
 			})
+			//.setTween(TweenMax.fromTo(textToExpand, 1, {autoAlpha: 0}, {autoAlpha: 1, ease: Power1.EaseInOut} ) )
 			.setClassToggle(this, 'is-inView')
 			.addTo(controller);
 
@@ -148,5 +234,53 @@ $(document).ready(function() {
 
 	}
 
+/*
+	var slider = $('.content-slider'),
+		item = slider.find('.slider-block'),
+		activItem = slider.find('.slide-activ'),
+		navPrev = $('.nav-prev'),
+		navNext = $('.nav-next'),
+		slideCount = item.length,
+		totalItems = $('.count-total'),
+		inViewItem = $('.count-passed'),
+		currentCount = 1,
+		currentSlideId = 0,
+		prevSlideId,
+		isPlaying = false;
+
+	var slider = {
+
+		init: function() {
+
+			TweenMax.set(item.not(activItem), {autoAlpha: 0} );
+			TweenMax.set(navPrev, {autoAlpha: 0.2} );
+
+			navPrev.on('click', function(e) {
+
+				e.preventDefault();
+				goToPrev();
+
+			});
+
+			navNext.on('click', function(e) {
+
+				e.preventDefault();
+				goToNext();
+
+			});
+		},
+		goToNext: function() {
+
+		},
+		goToPrev: function() {
+
+		},
+		navigation: function() {
+
+		},
+	};
+
+	slider.init();
+*/
 });
 
